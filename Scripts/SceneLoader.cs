5927bb374804738614c5143c91ca69930e8ca9ef
Xuzon Garrick <xuzon69@gmail.com>
2019-11-03 22:51:22 +0100

    Added red world, TMPRO and scene loader

---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    protected string levelName = "victory_world";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+        }
+    }
+}
